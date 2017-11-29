89524267d9046b07ea7959ce2e318e940938dceb
tongliu <tongliucn@gmail.com>
2017-11-29 14:44:10 -0500

    add several printf functions

--- sorting.c
+++ sorting.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define SIZE 100000
+#define SIZE 200000
 
 void mergesort(int a[], size_t top, size_t bottom);
 void merge(int a[], size_t top, size_t bottom);
@@ -74,11 +74,52 @@ float secondsHeap = (float)(endHeap - startHeap) / CLOCKS_PER_SEC;
 float secondsQuick = (float)(endQuick - startQuick) / CLOCKS_PER_SEC;
 float secondsBubble = (float)(endBubble - startBubble) / CLOCKS_PER_SEC;
 
+/* Print out the sorted results
+printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+printf("Before sorting, the array is as follows:\n");
+
+int count;
+for(count =0; count < SIZE; count++)
+{
+	printf("%d ", array1[count]);
+}
+printf("\nAfter sorting:");
+printf("\n---------------------------------");
+
+printf("\nMergesort Time: %f, the sorted result is:\n", secondsMerge);
+for(count =0; count < SIZE; count++)
+{
+        printf("%d ", arrayMerge[count]);
+}
+printf("\n---------------------------------");
+
+printf("\nInsertionsort Time: %f, the sorted result is\n", secondsInsertion);
+for(count =0; count < SIZE; count++)
+{
+        printf("%d ", arrayInsertion[count]);
+}
+printf("\n---------------------------------");
+
+printf("\nHeapsort Time: %f, the sorted result is\n", secondsHeap);
+for(count =0; count < SIZE; count++)
+{
+        printf("%d ", arrayHeap[count]);
+}
+printf("\n---------------------------------");
+
+printf("\nBubblesort Time: %f, the sorted result is\n", secondsBubble);
+for(count =0; count < SIZE; count++)
+{
+        printf("%d ", arrayBubble[count]);
+}
+printf("\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+printf("\n");
+//printf("Quicksort Time: %f\n", secondsQuick);
+*/
 printf("Mergesort Time: %f\n", secondsMerge);
 printf("Insertionsort Time: %f\n", secondsInsertion);
 printf("Heapsort Time: %f\n", secondsHeap);
 printf("Bubblesort Time: %f\n", secondsBubble);
-//printf("Quicksort Time: %f\n", secondsQuick);
 }
 
 void mergesort(int a[], size_t top, size_t bottom)
@@ -194,45 +235,33 @@ void Swap( int *num_a, int *num_b )
     *num_a = temp;  
 }  
   
-// array是待调整的堆数组,i是待调整的数组元素的位置,nlength是数组的长度  
 void HeapAdjust(int array[], int i, int nLength)  
 {  
     int nChild, nTemp;  
     for (nTemp = array[i]; 2 * i + 1 < nLength; i = nChild)  
     {  
-        // nChild:左子结点的位置是 父结点位置 * 2 + 1  nChild + 1： 右子结点   
         nChild = 2 * i + 1;  
-        // 得到子结点中较大的结点  
         if (nChild != nLength - 1 && array[nChild + 1] > array[nChild])  
             ++nChild;  
-        // 如果较大的子结点大于父结点那么把较大的子结点往上移动,替换它的父结点  
         if (nTemp < array[nChild])  
         {  
             array[i] = array[nChild];  
         }  
-        else  // 否则退出循环  
-        {  
+        else         {  
             break;  
         }  
     }  
-    // 最后把需要调整的元素值放到合适的位置  
     array[i] = nTemp;  
 }  
-// 堆排序算法  
 void HeapSort(int array[], int length)  
 {  
-    // 调整序列的前半部分元素,（即每个有孩子的节点）调整完之后是一个大顶堆，第一个元素是序列的最大的元素  
     for (int i = length / 2 - 1; i >= 0; --i)  
     {  
         HeapAdjust(array, i, length);  
     }  
-    // 从最后一个元素开始对序列进行调整,不断的缩小调整的范围直到第一个元素  
     for (int i = length - 1; i > 0; --i)  
     {  
-        // 把第一个元素和当前的最后一个元素交换,  
-        // 保证当前的最后一个位置的元素都是在现在的这个序列之中最大的  
         Swap(&array[0], &array[i]);  
-        // 不断缩小调整heap的范围,每一次调整完毕保证第一个元素是当前序列的最大值  
         HeapAdjust(array, 0, i);  
     }  
 }  
